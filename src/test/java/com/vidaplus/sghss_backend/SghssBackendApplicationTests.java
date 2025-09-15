fb73712c95f3853d1b8587228774b283e3630286
ranieriiuri <iuriranierioliveira@gmail.com>
2025-09-15 14:05:16 -0300

    Projeto inicial Spring Boot SGHSS-VidaPlus

---
+++ src/test/java/com/vidaplus/sghss_backend/SghssBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidaplus.sghss_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SghssBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
